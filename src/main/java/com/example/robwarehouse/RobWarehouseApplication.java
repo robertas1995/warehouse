7637ced40d1776eafa7d99d0a9ccb7c644354191
robertas miskinis <robertukas.2a@gmail.com>
2021-12-09 21:13:48 +0200

    Warehouse App FIRST DAY

---
+++ src/main/java/com/example/robwarehouse/RobWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.robwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobWarehouseApplication.class, args);
+    }
+
+}
